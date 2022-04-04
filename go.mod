a0cc3435161949b53f08a74fd91b026b1b577ab8
nadysaptra <saputranady@gmail.com>
2022-04-04 16:09:36 +0700

    feat: add go modules

---
+++ go.mod
@@ -0,0 +1,15 @@
+module azure-labs-test
+
+go 1.13
+
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/gofiber/fiber/v2 v2.18.0
+	github.com/golang-jwt/jwt/v4 v4.0.0
+	github.com/joho/godotenv v1.4.0
+	github.com/klauspost/compress v1.13.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/sys v0.0.0-20211102192858-4dd72447c267 // indirect
+	gorm.io/driver/mysql v1.1.3
+	gorm.io/gorm v1.22.2
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+
+	"azure-labs-test/app/routers"
+	"azure-labs-test/config"
+	"azure-labs-test/database"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := fiber.New()
+
+	config.InitDB()
+	db := config.DB
+	database.InitMigration(db)
+
+	routers.Init(app)
+	app.Listen(":" + os.Getenv("APP_PORT"))
+}
